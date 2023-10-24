05c35a7731599445fc2f15f444e109cb88db03c7
smart <smart@smhrd>
2023-10-24 09:33:08 +0900

    드리뮤 첫 커밋

---
+++ gitDriMyu/src/main/java/kr/board/entity/touristSpots.java
@@ -0,0 +1,39 @@
+package kr.board.entity;
+
+public class touristSpots {
+	
+	private String touristName;
+	private String location;
+	private String description;
+	private String keyWord;
+	
+	
+	
+	public String getTouristName() {
+		return touristName;
+	}
+	public void setTouristName(String touristName) {
+		this.touristName = touristName;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getKeyWord() {
+		return keyWord;
+	}
+	public void setKeyWord(String keyWord) {
+		this.keyWord = keyWord;
+	}
+	
+	
+
+}
